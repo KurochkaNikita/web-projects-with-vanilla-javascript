78ea6226327553fa4f006c53e66d9edf3dd2bf42
nikita <freenikita95@gmail.com>
2020-04-24 19:08:16 +0300

    finished js logic

--- Expense Tracker/index.js
+++ Expense Tracker/index.js
@@ -6,14 +6,41 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-    {id: 1, text: 'Flower', amount: -20},
-    {id: 2, text: 'Salary', amount: 300},
-    {id: 3, text: 'Cinema', amount: -20},
-    {id: 4, text: 'Ace-cream', amount: -5},
-];
+const localStorageTransactions = JSON.parse(localStorage.getItem('transaction'))
 
-let transactions = dummyTransactions;
+let transactions = localStorageTransactions || [];
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert("Please add text and amount")
+    } else {
+        const transaction = {
+            id: generateId(),
+            text: text.value,
+            amount: parseInt(amount.value)
+        }
+
+        transactions.push(transaction)
+        addTransactionsDOM(transaction)
+        updateValues()
+        text.value = ''
+        amount.value = ''
+
+        updateLocalStorage()
+    }
+}
+
+function removeTransaction(transactionId) {
+    transactions = transactions.filter(({id}) => id !== +transactionId)
+    init()
+    updateLocalStorage()
+}
+
+function generateId() {
+    return Math.floor(Math.random() * 10000000);
+}
 
 function addTransactionsDOM(transaction) {
     const sign = transaction.amount < 0 ? '-' : "+";
@@ -24,16 +51,41 @@ function addTransactionsDOM(transaction) {
 
     item.innerHTML = `
         ${transaction.text} <spam>${sign}${Math.abs(transaction.amount)}</spam>
-        <button class="delete-btn">X</button>
+        <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
     `
 
     list.appendChild(item)
+}
+
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts.filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const outcome = amounts.filter(item => item < 0)
+        .reduce((acc, item) => (acc -= item), 0)
+        .toFixed(2);
+
+    balance.innerText = `$${total}`;
+    moneyPlus.innerText = `$${income}`
+    moneyMinus.innerText = `$${outcome}`
 }
 
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(item => addTransactionsDOM(item))
+    updateValues()
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('transaction', JSON.stringify(transactions))
 }
 
 init()
+
+form.addEventListener('submit', addTransaction)
